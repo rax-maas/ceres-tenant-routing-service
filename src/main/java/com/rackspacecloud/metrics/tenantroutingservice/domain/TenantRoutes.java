bd80c991fc44aace27a6da8dec710664aacca0c7
Adam <harageth@gmail.com>
2019-01-10 19:18:45 -0800

    make spring data see the data we are putting into redis

--- src/main/java/com/rackspacecloud/metrics/tenantroutingservice/domain/TenantRoutes.java
+++ src/main/java/com/rackspacecloud/metrics/tenantroutingservice/domain/TenantRoutes.java
@@ -1,18 +1,22 @@
 package com.rackspacecloud.metrics.tenantroutingservice.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.rackspacecloud.metrics.tenantroutingservice.model.IngestionRoutingInformationInput;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RedisHash("routing-metadata")
 @Data
 public class TenantRoutes {
     @Id
     private String tenantId;
+    @JsonProperty("routes")
     private Map<String, TenantRoute> routes;
 
     public TenantRoutes() {
