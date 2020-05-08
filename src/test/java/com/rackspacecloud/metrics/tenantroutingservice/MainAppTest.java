883048ab97a002bc263f4d792140b3b406e5f8c2
Geoff Bourne <geoff.bourne@rackspace.com>
2020-05-08 09:48:52 -0500

    Consolidated application.properties into application.yaml (#15)

--- src/test/java/com/rackspacecloud/metrics/tenantroutingservice/MainAppTest.java
+++ src/test/java/com/rackspacecloud/metrics/tenantroutingservice/MainAppTest.java
@@ -1,11 +1,13 @@
 package com.rackspacecloud.metrics.tenantroutingservice;
 
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("development")
 public class MainAppTest {
     @Test
     public void applicationContextTest() {
-        // There is no assert here. It's mainly to test applicationContext loading.
-        TenantRoutingServiceApplication.main(new String[] {});
     }
 }
